41ae40baaeeb99676097c19d0dbc7359573d8e0a
Gorbatenko Vitaliy <gorvit.dp@gmail.com>
2021-02-21 22:26:36 +0200

    added lambda test

--- src/test/java/io/qaguru/tests/IssueSearchTest.java
+++ src/test/java/io/qaguru/tests/IssueSearchTest.java
@@ -6,6 +6,7 @@ import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 import static org.openqa.selenium.By.linkText;
 
 public class IssueSearchTest {
@@ -25,6 +26,31 @@ public class IssueSearchTest {
         $(byText("Issues")).click();
 
         $("#issue_" + ISSUE_NUMBER).should(exist);
+    }
+
+    @Test
+    void issueSearchTestLambda() {
+        step("Открываем главную страницу", () -> {
+            open("https://github.com/");
+        });
+
+        step("Поиск репозитория " + REPOSITORY, () -> {
+            $(".header-search-input").click();
+            $(".header-search-input").setValue(REPOSITORY);
+            $(".header-search-input").pressEnter();
+        });
+
+        step("Переход в репозиторий " + REPOSITORY, () -> {
+            $(linkText(REPOSITORY)).click();
+        });
+
+        step("Переход в раздел Issues", () -> {
+            $(byText("Issues")).click();
+        });
+
+        step("Проверка существования issue #" + ISSUE_NUMBER, () -> {
+            $("#issue_" + ISSUE_NUMBER).should(exist);
+        });
     }
 
 }
